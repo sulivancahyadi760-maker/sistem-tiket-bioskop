43f3a90db4d9c9b192aaa6958bacb1f935589a66
sulivancahyadi760-maker <sulivancahyadi760@gmail.com>
2026-02-20 11:37:53 +0700

    Menambahkan Movie Controller dan Movie Service

--- src/controller/MovieController.java
+++ src/controller/MovieController.java
@@ -1,5 +1,33 @@
 package controller;
 
+import java.util.List;
+
+import model.Movie;
+import repository.MovieRepository;
+
 public class MovieController {
     //tambah hapus film
+
+    private MovieRepository movieRepo;
+    private List<Movie> seluruhMovie; 
+    
+    MovieController(MovieRepository movieRepo){
+        this.movieRepo = movieRepo;
+        this.seluruhMovie = this.movieRepo.getAllMovies();
+    }
+
+    public void addFilm(Movie movie){
+        if(!seluruhMovie.contains(movie)){
+            seluruhMovie.add(movie);
+        }
+    }
+
+    public void deleteFilm(String namaFilm){
+        for(Movie mv : seluruhMovie){
+            if(mv.getNamaFilm().equals(namaFilm)){
+                seluruhMovie.remove(mv);
+                break;
+            }
+        }
+    }
 }
--- src/service/MovieService.java
+++ src/service/MovieService.java
@@ -1,7 +1,26 @@
 package service;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import model.Movie;
+import repository.MovieRepository;
 
 public class MovieService {
     //logic filter film berdasarkan genrenya
+
+    private MovieRepository movieRepo;
+    private List<Movie> seluruhMovie; 
+
+    public MovieService(MovieRepository movieRepo){
+        this.movieRepo = movieRepo;
+        this.seluruhMovie = this.movieRepo.getAllMovies();        
+    }
+
+    public List<Movie> filterByGenre(String genre){
+        List<Movie> movieByGenre = seluruhMovie.stream().filter(movie -> movie.getGenre().equals(genre)).collect(Collectors.toList());
+        return movieByGenre;
+    }
     
 }
